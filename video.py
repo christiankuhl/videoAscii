4c71c0b8d0a607afa75ff0cfaf8f94c935e4b923
Christian Kuhl <christian.kuhl84@gmail.com>
2019-06-01 13:25:08 +0200

    Use ncurses and numpy.vectorize, Matrix colours

--- video.py
+++ video.py
@@ -1,79 +1,58 @@
 import cv2
 import os
+import numpy as np
+import curses
 
-########################################################################
-### SETTING ###
-SHOW_REAL_VIDEO = False   # Set this to True to get real camera video from cv2
-
-########################################################################
-
-
-def flattenList(inputList):
-    output = inputList[:]
-    while type(output[0]) == list:
-        temp = []
-        for sub in output:
-            temp += sub
-        output = temp[:]            
-    return output
-
-def convert_to_ascii(inputGrays):
-    # 17-long
-    order = [' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N']
-
-    outputArray = []
-
-    for row in inputGrays:
-        adjusted_row = [int(x/(255/16)) for x in row]
-        newRow = []
-        for i in adjusted_row:
-            newRow.append(order[i])
-        outputArray.append(newRow)
-
-    #print output
-    return outputArray
-
-def printArray(inputAsciiArray):
-    fullImage = []
-    for row in inputAsciiArray:
-        fullImage.append(''.join(row))
-    os.system("clear")
-    print('\n'.join(fullImage), end='')
+# Set this to True to get real camera video from cv2
+SHOW_REAL_VIDEO = False
 
 
+ASCII_MAP = dict(enumerate([' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o',
+                            'k', 'X', 'd', 'O', '0', 'K', 'N']))
+convert_to_ascii = np.vectorize(lambda x: ASCII_MAP[int(x/15.9375)])
 
-cap = cv2.VideoCapture(0)
+def main(stdscreen):
+    init_curses(stdscreen)
+    cap = cv2.VideoCapture(0)
+    quit = False
+    while not quit:
+        # Get screensize for reduction
+        screen_height, screen_width = stdscreen.getmaxyx()
+        # Capture frame-by-frame
+        # Get image data
+        ret, frame = cap.read()
+        # Our operations on the frame come here
+        # Convert data to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        if SHOW_REAL_VIDEO:
+            cv2.imshow('frame',gray)
+        #Reduce grayscale array to proper resolution
+        reduced = cv2.resize(gray, (int(screen_width), int(screen_height)))
+        #Plug in reduced resolution numpy array for ascii converter func
+        converted = convert_to_ascii(reduced)
+        # Output result
+        for row_num, row in enumerate(converted):
+            stdscreen.addstr(row_num, 0, ''.join(row[:-1]))
+        stdscreen.refresh()
+        # Test for quit signal
+        quit = (cv2.waitKey(1) & 0xFF == ord('q') or stdscreen.getch() == ord('q'))
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-while(True):
-    #Get screensize for reduction
-    screen_height,  screen_width = os.popen('stty size', 'r').read().split()
-    
-    # Capture frame-by-frame
-    #Get image data
-    ret, frame = cap.read()
+def init_curses(stdscreen):
+    # Do not echo characters to terminal
+    curses.noecho()
+    # No input buffer, make stdscreen.getch() nonblocking
+    curses.cbreak()
+    stdscreen.nodelay(1)
+    # Hide cursor
+    curses.curs_set(0)
+    # Matrix colors :)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    stdscreen.attron(curses.color_pair(1))
+    stdscreen.clear()
 
-    # Our operations on the frame come here
-    #Convert data to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-
-    # Display the resulting frame
-    if SHOW_REAL_VIDEO:
-        cv2.imshow('frame',gray)
-
-    #Reduce grayscale array to proper resolution
-    reduced = cv2.resize(gray, (int(screen_width), int(screen_height)))
-
-    #Plug in reduced resolution numpy array for ascii converter func
-    converted = convert_to_ascii(reduced)
-    printArray(converted)
-
-
-    print("")
-    #Test for break
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    curses.wrapper(main)
